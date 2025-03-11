843b2dd90a508a5be65d8169d9716054736ac4c1
gitwilma <wilma.haakansson@gmail.com>
2025-03-11 10:43:55 +0100

    design post form

---
+++ app/new/page.tsx
@@ -0,0 +1,30 @@
+export default function NewPostPage() {
+  return (
+    <main className="p-4">
+      <form className="grid gap-2">
+        <h2 className="text-2xl">Add new post</h2>
+        <input
+          className="bg-slate-50 rounded px-2 py-1"
+          name="title"
+          type="text"
+          placeholder="Add a title..."
+        />
+        <textarea
+          className="bg-slate-50 rounded px-2 py-1"
+          name="content"
+          placeholder="Add content..."
+          rows={8}
+        />
+        <input
+          className="bg-slate-50 rounded px-2 py-1"
+          name="author"
+          type="text"
+          placeholder="Add an author..."
+        />
+        <button className="px-2 py-1 rounded hover:bg-slate-100 cursor-pointer">
+          Save
+        </button>
+      </form>
+    </main>
+  );
+}
